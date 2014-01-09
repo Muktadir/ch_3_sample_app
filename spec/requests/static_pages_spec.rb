fbc35a1f914fa3641b48c92dea839efff66cca6c
Muktadir Hossain <your.email@example.com>
2014-01-10 03:49:34 +0600

    End of ch3

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,21 +1,10 @@
-=begin
-require 'spec_helper'
-
-describe "StaticPages" do
-  describe "GET /static_pages" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get static_pages_index_path
-      response.status.should be(200)
-    end
-  end
-end
-=end
-
 
 require 'spec_helper'
 
 describe "Static pages" do
+
+  let(:base_title) { "Sample App" }
+
 
   describe "Home page" do
 
@@ -26,7 +15,7 @@ describe "Static pages" do
 
     it "should have the title 'Home'" do
       visit '/static_pages/home'
-      expect(page).to have_title('Home')
+      expect(page).to have_title("#{base_title}|Home")
     end
   end
 
@@ -39,7 +28,7 @@ describe "Static pages" do
 
     it "should have the right title." do
       visit '/static_pages/help'
-      expect(page).to have_title('Help')
+      expect(page).to have_title("#{base_title}|Help")
     end
   end
 
@@ -52,7 +41,20 @@ describe "Static pages" do
 
     it "should have the right title" do
       visit '/static_pages/about'
-      expect(page).to have_title('About Us')
+      expect(page).to have_title("#{base_title}|About Us")
+    end
+  end
+
+  describe "Contact page" do
+
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("#{base_title}|Contact")
     end
   end
 end
