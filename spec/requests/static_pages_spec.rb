c8620fadd2e48d46f419c0b32ce2bb0a87c89d37
Muktadir Hossain <your.email@example.com>
2014-03-05 15:28:17 +0600

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,59 +3,41 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  subject {page}
+
   let(:base_title) { "Sample App" }
 
 
   describe "Home page" do
 
-    it "should have the content 'Ch3 Sample App'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('Ch3 Sample App')
-    end
+    before {visit root_path}
 
-    it "should have the title 'Home'" do
-      visit '/static_pages/home'
-      expect(page).to have_title("#{base_title}|Home")
-    end
+    it {should have_content ('Sample App')}
+    it {should have_title ('Home')}
   end
 
   describe "Help page" do
 
-    it "should have the content 'Help'" do
-      visit '/static_pages/help'
-      expect(page).to have_content('Help')
-    end
+    before {visit help_path}
 
-    it "should have the right title." do
-      visit '/static_pages/help'
-      expect(page).to have_title("#{base_title}|Help")
-    end
+    it {should have_content ('Help')}
+    it {should have_title ('Help')}
   end
 
   describe "About page" do
 
-    it "should have the content 'About us'" do
-      visit '/static_pages/about'
-      expect(page).to have_content('About Us')
-    end
+    before {visit about_path}
 
-    it "should have the right title" do
-      visit '/static_pages/about'
-      expect(page).to have_title("#{base_title}|About Us")
-    end
+    it {should have_content ('About')}
+    it {should have_title ('About')}
   end
 
   describe "Contact page" do
 
-    it "should have the content 'Contact'" do
-      visit '/static_pages/contact'
-      expect(page).to have_content('Contact')
-    end
+    before {visit contact_path}
 
-    it "should have the right title" do
-      visit '/static_pages/contact'
-      expect(page).to have_title("#{base_title}|Contact")
-    end
+    it {should have_content ('Contact')}
+    it {should have_title ('Contact')}
   end
 end
 
